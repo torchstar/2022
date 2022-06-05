c6aa9976c6f6e3fe404cb584fd87c74d13746a8c
torchstar <13844826204@163.com>
2022-06-05 09:38:48 +0800

    设计模式学习

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,13 +10,14 @@ import java.io.IOException;
  * @date 2022年06月05日 1:28
  */
 public class Test {
-    public static void main(String[] args) {
-
+    @org.junit.Test
+    public void Test001() {
         try {
             Document post = Jsoup.connect("https://www.zhipin.com/xian/?ka=header-home").get();
             System.out.println(post.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
