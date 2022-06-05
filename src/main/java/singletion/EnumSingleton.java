05407720ef97724a01547c96a22b44e2d6fa5f05
torchstar <13844826204@163.com>
2022-06-05 11:57:31 +0800

    设计模式学习

---
+++ src/main/java/singletion/EnumSingleton.java
@@ -0,0 +1,20 @@
+package singletion;
+
+public enum EnumSingleton {
+    INSTANCE;
+
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public static EnumSingleton getInstance(){
+        return INSTANCE;
+    }
+
+}
--- src/main/java/singletion/LazyStsticInnerClassSingleton.java
+++ src/main/java/singletion/LazyStsticInnerClassSingleton.java
@@ -3,6 +3,21 @@ package singletion;
 /**
  * @author torchstar
  * @date 2022年06月05日 9:35
+ * description 优点：写法优雅，利用了java本身语法特点，性能高，避免了内存浪费
+ *              缺点：能够被反射破坏（可解决，写法不优雅）
  */
 public class LazyStsticInnerClassSingleton {
+    private LazyStsticInnerClassSingleton(){
+//        if(LazyHolder.INSTANCE != null){
+//            throw new RuntimeException("不允许非法访问！！！");
+//        }
+    }
+
+    private static LazyStsticInnerClassSingleton getInstance(){
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder{
+        private static final LazyStsticInnerClassSingleton INSTANCE = new LazyStsticInnerClassSingleton();
+    }
 }
